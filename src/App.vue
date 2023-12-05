4741167dc668ccbbf0ed921497b8595cc7216a52
Zazzaro <sassaro01cristian@gmail.com>
2023-12-05 18:48:32 +0100

    Fixed searchbar

--- src/App.vue
+++ src/App.vue
@@ -10,35 +10,61 @@ export default {
   },
   data() {
     return {
-      store: {},
+      store: {
+        apiUrl: 'https://api.themoviedb.org/3/discover/movie',
+        movies: [],
+        filteredMovies: [],
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYmZjOGI1YjRmOTI5ZTg1NmU3NjEwMzI5NmVkNGI5MSIsInN1YiI6IjY1NmRiNWM2NjUxN2Q2MDBjYzQyNzQxOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YHf6cFUeM_GJ84vuQOIaPntRdc18F7Vhw-P1tmlgpfc',
+        }
+      },
       searchTerm: "",
+      filteredData: [],
     };
   },
   methods: {
     async loadData() {
       try {
-        this.store.movies = await fetchData();
-        console.log(this.store);
+        this.store.movies = await fetchData(this.searchTerm);
+        this.filteredData = JSON.parse(JSON.stringify(this.store.movies.results));
       } catch (error) {
         console.error("Failed to load data:", error);
       }
     },
     handleSearch(searchTerm) {
       this.searchTerm = searchTerm;
-      this.loadData()
-    },
+      this.filteredData = this.store.movies.results.filter((el) => {
+        return el.title.toLowerCase().includes(this.searchTerm.toLowerCase());
+      });
+      return this.filteredData;
+    }
+
   },
   mounted() {
     this.loadData();
   },
 };
+
 </script>
 
 <template>
   <div>
     <searchBar @search="handleSearch" />
-    <moviesCard v-if="store.movies && store.movies.results" :info="store.movies.results" searchTerm="searchTerm" />
+    <div class="flex flex-wrap w-8/12 m-auto" v-if="store.movies && store.movies.results">
+      <moviesCard v-for="movie in filteredData" :info="movie" :key="movie.id" />
+    </div>
+
+    <div v-else>
+      Loading movies...
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@use './styles/general.scss';
+</style>
+
+<style lang="scss" scoped>
+@use './styles/partials/_variables.scss';
+</style>
--- src/components/moviesCard.vue
+++ src/components/moviesCard.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-        {{ console.log() }}
-        <div v-for="movie in info" :key="movie.id">
-            <div class="card w-96 h-44 bg-gradient-to-tr from-yellow-400 to-red-700 p-10 m-5">
-                <p>{{ movie.title }}</p>
-                <p v-if="searchTerm">{{ searchTerm }}</p>
-            </div>
+    <div class="w-64 bg-red-700 p-2 m-10 relative" @mouseover="showDetails = true" @mouseout="showDetails = false">
+        <img :src="'https://image.tmdb.org/t/p/w500/' + info.poster_path" alt="" class="object-cover" v-show="!showDetails">
+
+        <div class="p-5 absolute top-0 left-0 w-full h-full overflow-scroll  bg-black bg-opacity-70 text-white"
+            v-show="showDetails">
+            <p>Title: {{ info.title }}</p>
+            <p>Language: {{ info.original_language }}</p>
+            <p>Vote: {{ info.vote_average }}</p>
+            <p>{{ info.overview }}</p>
         </div>
     </div>
 </template>
@@ -16,11 +18,33 @@ export default {
     props: {
         info: Object,
         searchTerm: String,
-    }
+    },
+    data() {
+        return {
+            showDetails: false,
+        };
+    },
 };
 </script>
   
 <style scoped>
-/* Stili per la card */
+img {
+    width: 100%;
+    height: 300px;
+    object-fit: contain;
+    transition: opacity 0.3s ease;
+}
+
+.p-5 {
+    display: none;
+}
+
+.w-64:hover .p-5 {
+    display: block;
+}
+
+.w-64:hover img {
+    opacity: 0;
+}
 </style>
   
